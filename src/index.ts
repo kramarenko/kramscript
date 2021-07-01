bbdf4bab361d07904ba9b101215860b93389eff9
kramarenko <vasil@kramarenko.com.ua>
2021-07-01 21:58:09 +0300

    draft for AST

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('init');
+import Lexer from './Lexer';
+import Parser from './Parser';
+
+const code = `
+    сумма РАВНО 5 МИНУС 9;
+    суммадва РАВНО 0 МИНУС 6;
+    КОНСОЛЬ сумма;
+    КОНСОЛЬ суммадва;
+    КОНСОЛЬ сумма МИНУС суммадва ПЛЮС ( 5 ПЛЮС 3 );
+    `;
+
+const lexer = new Lexer(code);
+
+lexer.lexAnalysis();
+
+console.log(lexer.tokenList);
